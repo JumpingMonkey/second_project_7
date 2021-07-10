5325e2e8596313362237dfa86a974861c86eb083
AlexMorozov <morozovprav@gmail.com>
2021-07-10 19:07:34 +0300

    main page fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const less = require("gulp-less");
+const imagemin = require('gulp-imagemin');
+
+gulp.task('imagemin', () => {
+  return gulp.src('source/images/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('build/images'))
+});
 
 gulp.task('html', () => {
   return gulp.src('source/*.html')
@@ -21,9 +28,10 @@ gulp.task('watch', () => {
     }
   });
 
-  gulp.watch('./source/*.html', gulp.series('html'))
+  gulp.watch('./source/*.html', gulp.series('html'));
+  gulp.watch('./source/images/*', gulp.series('imagemin'));
   gulp.watch("./source/*.html").on('change', browserSync.reload);
 });
 
 //Таск по умолчанию, Запускает del, styles, scripts и watch
-gulp.task('start', gulp.series('watch', 'html'));
+gulp.task('start', gulp.series('watch', 'html', 'imagemin'));
